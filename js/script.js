efbb5df7370de2a8bf81284c6b84420b9652d6c4
jaw_breaker <muhammadh1445@gmail.com>
2023-02-15 08:22:14 +0530

    interactive update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
-document.querySelector('.share-img-btn').addEventListener('click', function (){
-    let tooltip = document.querySelector('.tooltip')
-    let button_container = document.querySelector('.share-img-btn')
-    let button_img = document.querySelector('.share')
+let tooltip = document.querySelector('.tooltip')
+let button_container = document.querySelector('.share-img-btn')
+
+function toggleShare(){
     tooltip.classList.toggle('hidden')
     button_container.classList.toggle('active')
-    button_img.classList.toggle('active')
-})
+}
+
+
+
+addEventListener('keydown', function(event){
+    keypressed = event.key
+    if (keypressed === 'Escape') {
+        tooltip.classList.add('hidden');
+        button_container.classList.remove('active');
+    }
+});
+
+addEventListener('click', function(event) {
+    if (event.target.closest('.tooltip')) {
+        return
+    } else if (event.target.closest('.share-img-btn')){
+        return
+    } else {
+        tooltip.classList.add('hidden')
+        button_container.classList.remove('active')
+    }
+});
+
+
